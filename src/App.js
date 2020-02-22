5494fed389828507966ee099d1a68fcf60a31ced
tsinat08 <tsinat.weldetnesae@gmail.com>
2020-02-22 15:41:12 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <table>
+        <tbody>
+        <tr>
+          <td><img src/></td>
+          <td>Cell 2</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
