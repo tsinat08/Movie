950227b9095d2b7050a301ffb72bf6e48cc23658
tsinat08 <tsinat.weldetnesae@gmail.com>
2020-03-05 18:28:30 -0500

    set Movie

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from '../src/Components/Header/Header'
 import Search from '../src/Components/Search/Search'
+import Movie from "./Components/Movie/Movie";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Search/>
+      <Movie/>
     </div>
   );
 }
---
+++ src/Components/Movie/Movie.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Movie.css';
+
+
+class Movie extends React.Component{
+    render() {
+        return(
+            <div className="Movie">
+                <h2>Movies</h2>
+
+            </div>
+        )
+    }
+}
+
+export default Movie;
