c6e6a05988de632cf3b090fce93d3bb63a939e1d
tsinat08 <tsinat.weldetnesae@gmail.com>
2020-03-08 16:10:40 -0400

    set Movie

--- src/App.js
+++ src/App.js
@@ -5,14 +5,24 @@ import Header from '../src/Components/Header/Header'
 import Search from '../src/Components/Search/Search'
 import Movie from "./Components/Movie/Movie";
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Search/>
-      <Movie/>
-    </div>
-  );
+
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            movie: [],
+            searchTerm:''
+        }
+    }
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                <Search/>
+                <Movie/>
+            </div>
+        );
+    }
 }
 
 export default App;
