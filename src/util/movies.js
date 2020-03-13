82b40846dc3df5115a295100249666e646688d51
tsinat08 <tsinat.weldetnesae@gmail.com>
2020-03-13 16:24:12 -0400

    and apikey

---
+++ src/util/movies.js
@@ -0,0 +1,14 @@
+const apiKey=''
+
+const Movies = {
+    searchMovies (term){
+        return fetch (`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${term}`)
+            .then(data => data.json())
+            .then(data => {
+                console.log(data);
+                this.searchMovies({movies: [... data.results]})
+            })
+    }
+}
+
+export default Movies
