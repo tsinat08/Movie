b7c2af7d6f938cf48cc712cf14c619b6fff1e15f
tsinat08 <tsinat.weldetnesae@gmail.com>
2020-03-19 10:52:42 -0400

    fix search button

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -4,15 +4,28 @@ import './Search.css';
 class Search extends React.Component{
     constructor(props){
         super(props);
+        this.state={
+            term:''
+        }
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleSearch=this.handleSearch.bind(this);
 
     }
 
+    handleTermChange(event){
+        this.setState({term: event.target.value})
+    }
+
+    handleSearch(event){
+        this.props.searchDatabase(this.state.term);
+        event.preventDefault();
+    }
 
     render() {
         return(
             <div className="search">
-                     <input  placeholder="Search for movie or tv show" onChange={this.handleChange}/>
-                     <button >SEARCH</button>
+                     <input  placeholder="Search for movie or tv show" onChange={this.handleTermChange}/>
+                     <button onClick={this.handleSearch}>SEARCH</button>
             </div>
         )
     }
