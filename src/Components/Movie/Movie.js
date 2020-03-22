17f69ea14c390331c497073c8f371c4c4ea8a696
tsinat08 <tsinat.weldetnesae@gmail.com>
2020-03-21 23:44:35 -0400

    fixing error with poster path === null

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import './Movie.css';
+import noImage from './no image.png'
 
 
 class Movie extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            imageSrc:this.props.results.poster_path
+
+        }
+        this.handleImage=this.handleImage.bind(this);
+    }
+
+    handleImage(src){
+            if (this.state.imageSrc === null) {
+                return noImage
+            }else {
+                return (`https://image.tmdb.org/t/p/w185_and_h278_bestv2${this.props.results.poster_path}`)
+            }
+    }
     render() {
         const {results}=this.props;
         return(
             <div className="movie">
                 <div className="image-container">
-                    <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${results.poster_path}`} alt={`${results.title}`}/>
+                    <img src={this.handleImage()}
+                         alt={`${results.title}`}/>
                 </div>
                 <div className="movie-overview">
                     <h2>{results.title}</h2>
