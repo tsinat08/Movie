3299adc4ac04ba6b42bbbc8f57460b9898487dd5
tsinat08 <tsinat.weldetnesae@gmail.com>
2020-03-31 15:39:02 -0400

    add search results counts

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -2,21 +2,38 @@ import React from 'react';
 import './SearchResults.css';
 
 
-class SearchResults extends React.Component{
+
+class SearchResults extends React.Component {
+    results = [{resultsType: 'Movies', resultsCount: this.props.moviesCount, id: 1},
+        {resultsType: 'TV Shows', resultsCount: this.props.tvCount, id: 2}]
+
+    // handleType(){
+    //     this.state.map(type=>{
+    //         return(this.state.resultsType)
+    //  })}
+    // handleCount(){
+    //     this.state.map(counts=> {
+    //            return (this.state.resultsCount)
+    // })}
     render() {
-        return(
-            !this.props.loading && !(this.props.movies && this.props.movies.length) && !(this.props.tv && this.props.tv.length)?
+        return (
+            !this.props.loading && !(this.props.movies && this.props.movies.length) && !(this.props.tv && this.props.tv.length) ?
                 <div className="SearchResults">
-                <h3>Search Results</h3>
-                <li className="select">
-                    <ul>Movies</ul>
-                    <span>{this.props.moviesCount}</span>
-                    <ul>TV Shows</ul>
-                    <span>{this.props.tvCount}</span>
-                </li>
-            </div> : null
+                    <h3>Search Results</h3>
+                    {Object.keys(this.results).map((type) => {
+                        return(
+                        <li className="select">
+                            <ul>{this.results[type].resultsType}</ul>
+                            <span>{this.results[type].resultsCount}</span>
+                            {/*<ul>TV Shows</ul>*/}
+                            {/*<span>{this.props.tvCount}</span>*/}
+                        </li>
+                    )})}
+                </div> : null
         )
     }
 }
+
+
 
 export default SearchResults;
