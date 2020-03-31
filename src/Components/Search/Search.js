1a455b8ef3a5b4e240d7685c4609263dcedce115
tsinat08 <tsinat.weldetnesae@gmail.com>
2020-03-30 21:39:09 -0400

    add search results box

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -32,7 +32,7 @@ class Search extends React.Component{
         return(
             <div className="search">
                      <input onKeyPress={this.handleKey} placeholder="Search for movie or tv show" onChange={this.handleTermChange}/>
-                     {/*<button onClick={this.handleSearch} >SEARCH</button>*/}
+                     <button onClick={this.handleSearch} >SEARCH</button>
             </div>
         )
     }
