604e37ff2724f2cb964f4a6af7a5a00924115b8a
tsinat08 <tsinat.weldetnesae@gmail.com>
2020-04-27 14:25:33 -0400

    adding selecting by type

--- src/util/movies.js
+++ src/util/movies.js
@@ -1,9 +1,8 @@
-const apiKey='';
+const apiKey='56b205b08ae40f18ccbbdc95cb94e6ce';
 
 export const Movies = {
     searchMovies (term, page){
         if (!page || isNaN(page)) page=1;
-        console.log('atmovie', page)
         return fetch (`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&page=${page}&query=${term}`)
     }
 }
