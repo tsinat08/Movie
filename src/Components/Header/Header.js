bad52b2836a4ca3a590d994d62cbd430ae898718
twelde <tsinat.weldetnesae@gmail.com>
2021-01-28 22:20:11 -0500

    updating Bootstap

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,11 +7,14 @@ class Header extends React.Component {
     render() {
         return(
             <div className="Header">
+                <nav className="navbar navbar-dark">
+                    <a className="name navbar-brand" href="#">Search For A Movies</a>
+                </nav>
                 <table>
                     <tbody>
-                    <tr>
+                    <tr >
                         {/*<td className="pic"><img src={pic} alt='icon'/></td>*/}
-                        <td className="name"><h1>Search For A Movies</h1></td>
+                        {/*<td className="name"><h1>Search For A Movies</h1></td>*/}
                     </tr>
                     </tbody>
                 </table>
