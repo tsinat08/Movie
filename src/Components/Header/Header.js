a4f765d423ec59708b8d19304377b2cc51ab4aae
twelde <tsinat.weldetnesae@gmail.com>
2021-02-20 09:45:48 -0500

    Update CSS settings

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -13,7 +13,7 @@ class Header extends React.Component {
                 <table>
                     <tbody>
                     <tr >
-                        <td className="pic"><img src={pic} alt='icon'/></td>
+                        <td className="pic"><img src={pic} alt='icon' width="100" height="100" /></td>
                         <td className="name"><h1>TV & MOVIE SEARCH</h1></td>
                     </tr>
                     </tbody>
