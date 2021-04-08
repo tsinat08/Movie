7b1babd55f85ccac8f17a14a766e78a881d90305
twelde <tsinat.weldetnesae@gmail.com>
2021-04-08 14:35:43 -0400

    Adding Home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,12 +5,29 @@ import pic from './test.jpeg';
 class Home extends React.Component {
     render() {
         return (
-            <div className="card" style={{width: 10 + "rem"}}>
-                <img src={pic} className="card-img-top" alt="test"></img>
-                    <div className="card-body">
-                        <h5 className="card-title"></h5>
-                        <p className="card-text"></p>
+            <div>
+                <div className='column_header'>
+                    <h2>What's Popular</h2>
+                    <div className='selector_wrap'>
+                        <div className='selector'>
+                            <div className='anchor'><h3><a href='#' >Streaming</a></h3>
+                                <div className='background'></div></div>
+                            <div className='anchor'><h3><a href='#' >On TV</a></h3>
+                                <div className='background'></div></div>
+                            <div className='anchor'><h3><a href='#' >For Rent</a></h3>
+                                <div className='background'></div></div>
+                            <div className='anchor'><h3><a href='#' >In Theaters</a></h3>
+                                <div className='background'></div></div>
+                        </div>
                     </div>
+                </div>
+                <div className="card" style={{width: 10 + "rem"}}>
+                    <img src={pic} className="card-img-top" alt="test"></img>
+                        <div className="card-body">
+                            <h5 className="card-title"></h5>
+                            <p className="card-text"></p>
+                        </div>
+                </div>
             </div>
         )
     }
