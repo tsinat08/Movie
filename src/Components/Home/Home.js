9aaa4360942bd3996c8c4cda1cd0719578e655d5
twelde <tsinat.weldetnesae@gmail.com>
2021-04-14 04:40:53 -0400

    update home2

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -33,7 +33,7 @@ class Home extends React.Component {
                 <div className="card" style={{width: 10 + "rem"}}>
                     <img src={pic} className="card-img-top" alt="test"></img>
                         <div className="card-body">
-                            <h5 className="card-title"></h5>
+                            <h5 className="card-title">BatMan</h5>
                             <p className="card-text"></p>
                         </div>
                 </div>
